ad7379cff1414dbf14be0696300b66437dcef081
Lyte Onyema <lyte.onyema@gmail.com>
2023-07-17 16:28:52 +0100

    Floating numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,11 @@
 fun main(args: Array<String>) {
-    val maxByteValue = Byte.MAX_VALUE; // the value assigned to a byte variable cannot be greater than this value.
-    val minByteValue = Byte.MIN_VALUE;// the value assigned to a byte variable cannot be less than this value.
+    var myNumber: Number = 23.02 // Number can be any of whole numbers or decimal numbers
 
-    println("The maximum byte value is: $maxByteValue")
-    println("The minimum byte value is: $minByteValue")
+    // Decimal numbers
+    val myFloatNumber = 33.2f
+    val anotherNumber = 234.02
 
-    println("")
-
-    val maxShortValue = Short.MAX_VALUE; // the value assigned to a short variable cannot be greater than this value.
-    val minShortValue = Short.MIN_VALUE;// the value assigned to a short variable cannot be less than this value.
-
-    println("The maximum short number value is: $maxShortValue")
-    println("The minimum short number value is: $minShortValue")
-
-    println("")
-
-    val maxIntegerValue = Int.MAX_VALUE; // the value assigned to an integer variable cannot be greater than this value.
-    val minIntegerValue = Int.MIN_VALUE;// the value assigned to an integer variable cannot be less than this value.
-
-    println("The maximum integer value is: $maxIntegerValue")
-    println("The minimum integer value is: $minIntegerValue")
-
-    println("")
-
-    val maxLongValue = Long.MAX_VALUE; // the value assigned to a long variable cannot be greater than this value.
-    val minLongValue = Long.MIN_VALUE;// the value assigned to a long variable cannot be less than this value.
-
-    println("The maximum long number value is: $maxLongValue")
-    println("The minimum long number value is: $minLongValue")
-
-    val longNumber = 26L; // This is also another way to write long number.
+    // The difference in both is the precision in calculations (double is better)
+    println("This is a float: $myFloatNumber")
+    println("This is a double: $anotherNumber")
 }
