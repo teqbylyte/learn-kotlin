d2a09165ab8944c7259803a7f32e9d2573eabd1d
Lyte Onyema <lyte.onyema@gmail.com>
2023-07-17 16:05:02 +0100

    Whole numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,33 @@
 fun main(args: Array<String>) {
-    var greeting = "Hello" // Variable declared with var can be reassigned another value.
-    val name = "Lyte"
+    val maxByteValue = Byte.MAX_VALUE; // the value assigned to a byte variable cannot be greater than this value.
+    val minByteValue = Byte.MIN_VALUE;// the value assigned to a byte variable cannot be less than this value.
 
-    println("$greeting $name!")
+    println("The maximum byte value is: $maxByteValue")
+    println("The minimum byte value is: $minByteValue")
 
-    greeting = "Good morning"
+    println("")
 
+    val maxShortValue = Short.MAX_VALUE; // the value assigned to a short variable cannot be greater than this value.
+    val minShortValue = Short.MIN_VALUE;// the value assigned to a short variable cannot be less than this value.
 
-    println("$greeting $name!")
+    println("The maximum short number value is: $maxShortValue")
+    println("The minimum short number value is: $minShortValue")
+
+    println("")
+
+    val maxIntegerValue = Int.MAX_VALUE; // the value assigned to an integer variable cannot be greater than this value.
+    val minIntegerValue = Int.MIN_VALUE;// the value assigned to an integer variable cannot be less than this value.
+
+    println("The maximum integer value is: $maxIntegerValue")
+    println("The minimum integer value is: $minIntegerValue")
+
+    println("")
+
+    val maxLongValue = Long.MAX_VALUE; // the value assigned to a long variable cannot be greater than this value.
+    val minLongValue = Long.MIN_VALUE;// the value assigned to a long variable cannot be less than this value.
+
+    println("The maximum long number value is: $maxLongValue")
+    println("The minimum long number value is: $minLongValue")
+
+    val longNumber = 26L; // This is also another way to write long number.
 }
