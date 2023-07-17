297c6a99ec893f0f464bd3dd7fdb51af33b4a510
Lyte Onyema <lyte.onyema@gmail.com>
2023-07-17 15:39:36 +0100

    Project Setup and Variables

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var greeting = "Hello" // Variable declared with var can be reassigned another value.
+    val name = "Lyte"
+
+    println("$greeting $name!")
+
+    greeting = "Good morning"
+
+
+    println("$greeting $name!")
+}
