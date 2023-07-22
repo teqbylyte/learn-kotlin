739bd1f66bd0c6a897af2291c280061ee94e02b7
Lyte Onyema <lyte.onyema@gmail.com>
2023-07-22 23:17:31 +0100

    for loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,21 @@
 fun main(args: Array<String>) {
-    println(sum(2, 44, 34, 22, 23))
-}
 
-fun sum(vararg numbers: Int): String {
-    var result = 0;
-
-    for (number in numbers) {
-        println(number)
-        result += number;
+    // loop with a range of numbers
+    for (i in 1..10) {
+        println("i = $i")
     }
 
-    return  result.toString();
+    println("\n");
+
+    // in range but lesser than the last number
+    for (j in 2..<8) {
+        println("j = $j")
+    }
+
+    println("\n");
+
+    // in range from higher to lower and skip by 2
+    for (k in 30 downTo 20 step 2) {
+        println("k = $k")
+    }
 }
